3b62c29b73b1556ccafb03c5e4c2a41aad081476
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2017-10-11 14:02:22 +0900

    fix

--- redmine/projects.go
+++ redmine/projects.go
@@ -1,1 +1,22 @@
 package redmine
+
+import "time"
+
+type Project struct {
+	ID          int       `json:"id"`
+	Name        string    `json:"name"`
+	Identifier  string    `json:"identifier"`
+	Description string    `json:"description"`
+	Status      int       `json:"status"`
+	CreatedOn   time.Time `json:"created_on"`
+	UpdatedOn   time.Time `json:"updated_on"`
+}
+
+type Projects []Project
+
+type ReponseProjects struct {
+	Projects   `json:"projects"`
+	TotalCount int `json:"total_count"`
+	Offset     int `json:"offset"`
+	Limit      int `json:"limit"`
+}
