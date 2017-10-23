f43c1937626a84c463b0a0d84b1ad04032762209
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2017-10-23 13:11:42 +0900

    fix

--- redmine/filter.go
+++ redmine/filter.go
@@ -1,1 +1,21 @@
 package redmine
+
+import (
+	"strings"
+)
+
+func filterIssues(issues Issues, filters []string) Issues {
+	var objs Issues
+	for _, issue := range issues {
+		isExist := true
+		for _, filter := range filters {
+			isExist = isExist && strings.Contains(filter, issue.Subject) || strings.Contains(filter, issue.Description)
+		}
+
+		if isExist {
+			objs = append(objs, issue)
+		}
+	}
+
+	return objs
+}
